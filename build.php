517826c58a730ccd2f01a83fe21d685c9552054b
Gregwar <g.passault@gmail.com>
2013-09-05 00:15:01 +0200

    Ajout favicon

--- build.php
+++ build.php
@@ -23,6 +23,7 @@ $slidey->setTitle('FirstBot');
 
 // This will copy the directory "css" to the target directory
 $slidey->copy('img', 'img');
+$slidey->copy('favicon.ico');
 
 // Runs the build to the web directory
 $slidey->build();
